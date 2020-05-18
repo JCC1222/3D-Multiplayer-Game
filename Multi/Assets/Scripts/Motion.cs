93f10dcec2cb11d24d8f250a9f37c532e29bc9b8
C-Jones <JCC1222@users.noreply.github.com>
2020-05-19 00:03:56 +0200

    Adding Project

---
+++ Multi/Assets/Scripts/Motion.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Motion : MonoBehaviourPunCallbacks
+{
+
+    public float speed;
+    public float sprintModifier;
+    public float jumpForce;
+    public Camera normalCam;
+    public Transform weaponParent;
+    public GameObject cameraParent;
+    public Transform groundDetector;
+    public LayerMask ground;
+
+    public Rigidbody rig;
+
+    private Vector3 targetWeaponBobPosition;
+    private Vector3 weaponParentOrigin;
+
+    private float movementCounter;
+    private float idleCounter;
+
+    private float baseFOV;
+    private float sprintFOVModifier = 1.25f;
+
+    private void Start()
+    {
+        cameraParent.SetActive(photonView.IsMine);
+
+        if (!photonView.IsMine) gameObject.layer = 11 ;
+
+        baseFOV = normalCam.fieldOfView;
+        Camera.main.enabled = false;
+        rig = GetComponent<Rigidbody>();
+        weaponParentOrigin = weaponParent.localPosition;
+    }
+
+
+    void FixedUpdate()
+    {
+        if (!photonView.IsMine) return;
+
+        //Input
+        float t_hmove = Input.GetAxisRaw("Horizontal");
+        float t_vmove = Input.GetAxisRaw("Vertical");
+
+        //Controls
+        bool sprint = Input.GetKey(KeyCode.LeftShift);
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+
+        //States
+        bool isGrounded = Physics.Raycast(groundDetector.position, Vector3.down, 0.1f, ground);
+        bool isJumping = jump && isGrounded;
+        bool isSprinting = sprint && t_vmove > 0 && !isJumping && isGrounded;
+
+        //Jumping
+        if (isJumping)
+        {
+            rig.AddForce(Vector3.up * jumpForce);
+        }
+
+        //Head Bob
+        if (t_hmove == 0 && t_vmove == 0)
+        {
+            HeadBob(idleCounter, 0.025f, 0.025f);
+            idleCounter += Time.deltaTime;
+            weaponParent.localPosition = Vector3.Lerp(weaponParent.localPosition, targetWeaponBobPosition, Time.deltaTime * 2f);
+        }
+        else if (!isSprinting)
+        {
+            HeadBob(movementCounter, 0.035f, 0.035f);
+            movementCounter += Time.deltaTime * 3f;
+            weaponParent.localPosition = Vector3.Lerp(weaponParent.localPosition, targetWeaponBobPosition, Time.deltaTime * 6f);
+        }
+        else
+        {
+            HeadBob(movementCounter, 0.15f, 0.075f);
+            movementCounter += Time.deltaTime * 7f;
+            weaponParent.localPosition = Vector3.Lerp(weaponParent.localPosition, targetWeaponBobPosition, Time.deltaTime * 8f);
+        }
+
+        //Movement
+        Vector3 t_direction = new Vector3(t_hmove, 0, t_vmove);
+        t_direction.Normalize();
+         
+        float t_adjustedSpeed = speed;
+        if (isSprinting) t_adjustedSpeed *= sprintModifier;
+
+        Vector3 t_targetVelocity = transform.TransformDirection(t_direction) * t_adjustedSpeed * Time.deltaTime;
+        t_targetVelocity.y = rig.velocity.y;
+        rig.velocity = t_targetVelocity;
+
+
+        //Field of View
+        if (isSprinting)
+        {
+            normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV * sprintFOVModifier, Time.deltaTime * 8f);
+        }
+        else
+        {
+            normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV, Time.deltaTime * 8f);
+        }
+    }
+
+    #region Private Methods
+
+    void HeadBob(float p_z, float p_x_intensity, float p_y_intensity)
+    {
+        targetWeaponBobPosition = weaponParentOrigin + new Vector3(Mathf.Cos(p_z) * p_x_intensity,
+            Mathf.Sin(p_z * 2) * p_y_intensity, 0);
+    }
+
+    #endregion
+}
---
+++ Multi/Assets/Scripts/Weapon.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Weapon : MonoBehaviourPunCallbacks
+{
+    #region Variables
+
+    public Gun[] loadout;
+    public Transform weaponParent;
+    public GameObject bulletholePrefab;
+    public LayerMask canBeShot;
+
+    private float currentCooldown;
+    private int currentIndex;
+    private GameObject currentWeapon;
+
+    #endregion
+
+
+    #region MonoBehaviour Callbacks
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!photonView.IsMine) return;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { photonView.RPC("Equip", RpcTarget.AllViaServer, 0); }
+
+        if (currentWeapon != null)
+        {
+            if (Input.GetMouseButtonDown(0) && currentCooldown <= 0)
+            {
+                photonView.RPC("Shoot", RpcTarget.All);
+            }
+
+            //weapon position elasticity
+            currentWeapon.transform.localPosition = Vector3.Lerp(currentWeapon.transform.localPosition, Vector3.zero, Time.deltaTime * 4f);
+
+            //CoolDown
+            if (currentCooldown > 0)
+            {
+                currentCooldown -= Time.deltaTime;
+            }
+        }
+
+    }
+
+    #endregion
+
+
+    #region Private Methods
+
+    [PunRPC]
+    void Equip (int p_ind)
+    {
+        if (currentWeapon != null) Destroy(currentWeapon);
+
+        currentIndex = p_ind;
+
+        GameObject t_newWeapon = Instantiate(loadout[p_ind].prefab, weaponParent.position, weaponParent.rotation, weaponParent) as GameObject;
+        t_newWeapon.transform.localPosition = Vector3.zero;
+        t_newWeapon.transform.localEulerAngles = Vector3.zero;
+        t_newWeapon.GetComponent<Sway>().enabled = photonView.IsMine;
+
+        currentWeapon = t_newWeapon;
+    }
+
+    [PunRPC]
+    void Shoot()
+    {
+        Transform t_spawn = transform.Find("Cameras/Normal Camera");
+
+        //Bloom
+        Vector3 t_bloom = t_spawn.position + t_spawn.forward * 1000f;
+        t_bloom += Random.Range(-loadout[currentIndex].bloom, loadout[currentIndex].bloom) * t_spawn.up;
+        t_bloom += Random.Range(-loadout[currentIndex].bloom, loadout[currentIndex].bloom) * t_spawn.right;
+        t_bloom -= t_spawn.position;
+        t_bloom.Normalize();
+
+
+        //Raycast
+        RaycastHit t_hit = new RaycastHit();
+        if (Physics.Raycast(t_spawn.position, t_bloom, out t_hit, 1000f, canBeShot))
+        {
+            GameObject t_newHole = Instantiate(bulletholePrefab, t_hit.point + t_hit.normal * 0.001f, Quaternion.identity) as GameObject;
+            t_newHole.transform.LookAt(t_hit.point + t_hit.normal);
+            Destroy(t_newHole, 5f);
+
+            if (photonView.IsMine)
+            {
+                //Shooting Of Player
+                if(t_hit.collider.gameObject.layer == 11)
+                {
+                    //RPC Call to Damage Player
+                }
+            }
+        }
+
+        //Gun Fx
+        currentWeapon.transform.Rotate(-loadout[currentIndex].recoil, 0, 0);
+        currentWeapon.transform.position -= currentWeapon.transform.forward * loadout[currentIndex].kickback;
+        
+        //CoolDown
+        currentCooldown = loadout[currentIndex].firerate;
+    }
+
+    #endregion
+}
